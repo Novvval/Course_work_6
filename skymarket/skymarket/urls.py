989741ff7ff47e5aac11a4c437844f4687ad7d39
novvval <nekittt1997@gmail.com>
2023-01-02 15:49:18 +0500

    changes to authentication

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -3,6 +3,8 @@ from django.template.defaulttags import url
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # TODO здесь необходимо подклюючит нужные нам urls к проекту
 
@@ -11,6 +13,8 @@ urlpatterns = [
     path("api/redoc-tasks/", include("redoc.urls")),
     path("auth/", include('djoser.urls')),
     path("auth/", include("djoser.urls.jwt")),
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/refresh/", TokenRefreshView.as_view()),
     path("", include("ads.urls")),
     path("", include("users.urls"))
 ]
